991709437da414b49067c393bb6f5a60ff6829cd
Joseph Jacob <Joseph.Jacob@jpl.nasa.gov>
2021-08-24 21:38:39 -0700

    Added generic implementation for ADES abstract base class (to be replaced by specific implementation on each platform).

---
+++ flask_ades_wpst/ades_generic.py
@@ -0,0 +1,26 @@
+from flask_ades_wpst.ades_abc import ADES_ABC
+
+
+class ADES_Generic(ADES_ABC):
+
+    def deploy_proc(self, proc_spec):
+        return {}
+
+    def undeploy_proc(self, proc_spec):
+        return {}
+
+    def exec_job(self, job_spec):
+        return {}
+
+    def dismiss_job(self, job_spec):
+        return {}
+
+    def get_job(self, job_spec):
+        return {"status": "none"}
+
+    def get_job_results(self, job_spec):
+        return {"status": "none",
+                "links": [{"href": "https://mypath",
+                           "rel": "result",
+                           "type": "application/json",
+                           "title": "mytitle"}]}
