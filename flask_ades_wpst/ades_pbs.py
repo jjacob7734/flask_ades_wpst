644f73900dad3c82ef604cb142b77306860a4643
Joseph Jacob <Joseph.Jacob@jpl.nasa.gov>
2021-09-01 16:13:12 -0700

    Adjusted return values in the platform-specific ADES functions.

--- flask_ades_wpst/ades_pbs.py
+++ flask_ades_wpst/ades_pbs.py
@@ -4,23 +4,23 @@ from flask_ades_wpst.ades_abc import ADES_ABC
 class ADES_PBS(ADES_ABC):
 
     def deploy_proc(self, proc_spec):
-        return {}
+        return proc_spec
 
     def undeploy_proc(self, proc_spec):
-        return {}
+        return proc_spec
 
     def exec_job(self, job_spec):
-        return {}
+        return job_spec
 
     def dismiss_job(self, job_spec):
-        return {}
+        return job_spec
 
     def get_job(self, job_spec):
-        return {"status": "none"}
+        return job_spec
 
     def get_job_results(self, job_spec):
-        return {"status": "none",
-                "links": [{"href": "https://mypath",
+        res =  {"links": [{"href": "https://mypath",
                            "rel": "result",
                            "type": "application/json",
                            "title": "mytitle"}]}
+        return {**job_spec, **res}
