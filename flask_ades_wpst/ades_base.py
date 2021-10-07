58fb8cd0ebcca0e7e2d1b737b54d672d0ec2b062
Joseph Jacob <Joseph.Jacob@jpl.nasa.gov>
2021-10-07 09:36:41 -0700

    Fix source module for ADES_PBS import.

--- flask_ades_wpst/ades_base.py
+++ flask_ades_wpst/ades_base.py
@@ -14,7 +14,7 @@ class ADES_Base:
         elif self._platform == "Argo":
             from flask_ades_wpst.ades_argo import ADES_Argo as ADES_Platform
         elif self._platform == "PBS":
-            from flask_ades_wpst.ades_argo import ADES_PBS as ADES_Platform
+            from flask_ades_wpst.ades_pbs import ADES_PBS as ADES_Platform
         else:
             # Invalid platform setting.  If you do implement a new
             # platform here, you must also add it to the valid_platforms
